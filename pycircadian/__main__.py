ddf7c785983fba59343e051a5e003bf57480ed78
Bernard Cafarelli <bernard.cafarelli@gmail.com>
2023-11-03 17:38:19 +0100

    Prepare for system install
    Add systemd service, install fixes
    Also catch KeyboardInterrupt in main loop

--- pycircadian/__main__.py
+++ pycircadian/__main__.py
@@ -1,4 +1,5 @@
 from signal import signal, SIGUSR1
+from sys import exit as sysexit
 from threading import Event
 from pycircadian import config
 from pycircadian.all_checks import check_all_idle
@@ -15,8 +16,11 @@ def circadian_main():
 
     main_loop = Event()
     while True:
-        check_all_idle()
-        main_loop.wait(60)
+        try:
+            check_all_idle()
+            main_loop.wait(60)
+        except KeyboardInterrupt:
+            sysexit(0)
 
 
 if __name__ == "__main__":
