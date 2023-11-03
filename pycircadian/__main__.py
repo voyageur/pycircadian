3235ac7786fe5cea80b155c755d2957ab0a6d130
Bernard Cafarelli <bernard.cafarelli@gmail.com>
2023-11-03 17:13:21 +0100

    Add main loop and SIGUSR1 handling

--- pycircadian/__main__.py
+++ pycircadian/__main__.py
@@ -1,13 +1,22 @@
+from signal import signal, SIGUSR1
+from threading import Event
 from pycircadian import config
 from pycircadian.all_checks import check_all_idle
 
 
+def usr1_handler(signum, frame):
+    check_all_idle(False)
+
+
 def circadian_main():
     config.init_config()
-    # TODO
-    check_all_idle()
-    check_all_idle(True)
-    # idle_actions.run_action()
+
+    signal(SIGUSR1, usr1_handler)
+
+    main_loop = Event()
+    while True:
+        check_all_idle()
+        main_loop.wait(60)
 
 
 if __name__ == "__main__":
--- pycircadian/all_checks.py
+++ pycircadian/all_checks.py
@@ -4,44 +4,45 @@ from pycircadian import cpu_load, idle_actions, idle_sessions, net_connections, 
     process_block, sound_block
 
 
-def check_all_idle(print_status: bool = False):
-    # With print_status, run all checks and do not trigger action
+def check_all_idle(action: bool = True):
+    # With action = False, run all checks and do not trigger action
     log = getattr(logging, "warning")
-    if not print_status:
+    if action:
         log = getattr(logging, "info")
 
     sessions = idle_sessions.get_sessions()
     min_idle_tty = idle_sessions.get_min_idle_tty_sessions(sessions)
     log("Min TTY idle: {0}".format(min_idle_tty))
-    if min_idle_tty < config.main_config["idle_time"] and not print_status:
+    if min_idle_tty < config.main_config["idle_time"] and action:
         return False
     min_idle_x11 = idle_sessions.get_min_idle_x11_sessions(sessions)
     log("Min X11 idle: {0}".format(min_idle_x11))
-    if min_idle_x11 < config.main_config["idle_time"] and not print_status:
+    if min_idle_x11 < config.main_config["idle_time"] and action:
         return False
 
     blocking_processes = process_block.check_blocking_processes()
     log("Blocking processes: {0}".format(blocking_processes))
-    if blocking_processes and not print_status:
+    if blocking_processes and action:
         return False
     system_loaded = cpu_load.check_load_avg()
     log("System load above threshold: {0}".format(system_loaded))
-    if system_loaded and not print_status:
+    if system_loaded and action:
         return False
     active_net_connections = net_connections.check_net_connections()
     log("Active network connections: {0}".format(active_net_connections))
-    if active_net_connections and not print_status:
+    if active_net_connections and action:
         return False
     active_sound = sound_block.check_sound()
     log("Sound playing: {0}".format(active_sound))
-    if active_sound and not print_status:
+    if active_sound and action:
         return False
 
     can_idle = idle_actions.can_run_action()
     log("Can execute idle action: {0}".format(can_idle))
-    if not can_idle and not print_status:
+    if not can_idle and action:
         return False
 
-    if not print_status:
+    if action:
         logging.warning("System identified as idle")
-    return not print_status
+        idle_actions.run_action()
+    return action
